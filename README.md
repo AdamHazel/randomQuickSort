8063703fe8697cb12a43b2de089c3f7e8e989f98
Adam Richard Washington Hazel <arhazel@student.uia.no>
2024-10-14 11:53:07 +0200

    Adding solution

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # randomQuickSort
-A school project where we get to practice the randomised quick sort algorithm
+
+A school project where we get to practice the randomised quick sort algorithm.
+
+## For users
+
+### Explanation
+
+This program uses three classes:
+
+1. inputParser - this parses through the input string to remove basic errors, and places elements in a vector
+2. RandomQuickSort - sorts vector into an ordered vector
+3. printList - prints out vector as output to user
+
+### Limitations/Things to be aware of
+
+1. It is not possible to have expressions as elements. (e.g. 45-10 will be interpreted as 45). We assume that -45 is considered as a number, not an expression, and thus will be accepted by program.
+2. Duplicate symbols (commas, minus sign, decimal points) are removed automatically.
+3. Erroneous decimals will be ignored (e.g. the second decimal in 85.455.34 wil be ignored. The value will be 85.4553).
+4. Floats are rounded automatically according to system preferences
+5. No other symbols other than - , . are accepted. Other symbols return an error (and result in a restart of the program)
---
+++ main.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <random>
+
+namespace Constants {
+    constexpr char comma = ',';
+    constexpr char decimal = '.';
+    constexpr char minus = '-';
+}
+
+class controlFlags {
+public:
+    bool errorInProgram{};
+    bool programRunning{ true };
+
+    char tryAgain() {
+        std::cout << "Do you wish to order another list of values? \nType r to restart program or any other"
+                     " key to exit:" << std::endl;
+        std::string temp;
+        std::getline(std::cin >> std::ws, temp);
+        return static_cast<char>(std::toupper(static_cast<unsigned char>(temp[0])));
+    }
+
+    void programReset() {
+        errorInProgram = false;
+    }
+};
+
+class inputParser {
+private:
+    std::string input;
+
+    void stringGetter() {
+        std::cout << "\nPlease input list of values separated by commas. No need to use [] at the start or end"
+                     "of array:" << std::endl;
+        std::getline(std::cin >> std::ws, input);
+    }
+
+    bool isMinus(char a) {
+        return a == Constants::minus;
+    }
+
+    bool isValidSymbol(char a) {
+        return(a == Constants::comma || a == Constants::decimal || a == Constants::minus);
+    }
+
+    void removeDuplicatesAndCatchErrors(controlFlags &f) {
+        auto length = input.size();
+        for (int i = 0; i < length; i++) {
+            if(isspace(input[i])) {
+                input.erase(i, 1);
+                --i;
+                --length;
+            }
+
+            if (isdigit(input[i]))
+                continue;
+            else {
+                if(isValidSymbol(input[i])){
+                    if (i > 0 && input[i] == input[i-1]) {
+                        input.erase(i-1, 1);
+                        --length;
+                        --i;
+                    }
+                }
+                else {
+                    f.errorInProgram = true;
+                    std::cout << "ERROR: In valid symbol used. Restarting program..." << std::endl;
+                }
+            }
+        }
+    }
+
+    void parseToVector(std::vector<float>& vec, controlFlags &f) {
+        if (!f.errorInProgram) {
+            bool decimalUsed = false;
+            auto it = input.begin();
+            std::string temp;
+            while (it != input.end()) {
+                if(*it == Constants::comma) {
+                    if (!temp.empty()) {
+                        auto tempFloat = std::stof(temp);
+                        vec.push_back(tempFloat);
+                        temp.clear();
+                        decimalUsed = false;
+                    }
+                } else {
+                    if (isdigit(*it) || isMinus(*it) )
+                        temp.push_back(*it);
+                    else if (!decimalUsed) {
+                        temp.push_back(*it);
+                        decimalUsed = true;
+                    }
+                }
+                ++it;
+            }
+            if (!temp.empty()) {
+                auto tempFloat = std::stof(temp);
+                vec.push_back(tempFloat);
+                temp.clear();
+            }
+        }
+        else
+            return;
+    }
+
+public:
+    inputParser(std::vector<float>& vec, controlFlags &f) {
+        stringGetter();
+        removeDuplicatesAndCatchErrors(f);
+        parseToVector(vec, f);
+    }
+};
+
+class randomQuickSort {
+private:
+    int randomNumber(int min, int max) {
+        if (min <= max) {
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> distrib(min, max);
+            return distrib(gen);
+        }
+        else {
+            std::cout << "Random number generator not working. Returning -1" << std::endl;
+            return -1;
+        }
+    }
+
+    int partition(std::vector<float> &vec, int start, int end) {
+        float pivot = vec[end];
+
+        int partitionIndex = start;
+        for (int i = start; i < end; i++) {
+            if (vec[i] <= pivot) {
+                std::swap(vec[i], vec[partitionIndex]);
+                ++partitionIndex;
+            }
+        }
+        std::swap(vec[end], vec[partitionIndex]);
+        return partitionIndex;
+    }
+
+    int randomisedPartition(std::vector<float> &vec, int start, int end) {
+        int pivotIndex = randomNumber(start, end);
+        std::swap(vec[pivotIndex], vec[end]);
+        return partition(vec, start, end);
+    }
+
+    void QuickSort (std::vector<float> &vec, int start, int end) {
+        if (start < end) {
+            int pIndex {randomisedPartition(vec, start, end)};
+
+            QuickSort(vec,start, pIndex-1);
+            QuickSort(vec, pIndex+1, end);
+        }
+    }
+
+public:
+    randomQuickSort(std::vector<float> &vec) {
+        int start = 0;
+        int end = static_cast<int>(vec.size() - 1);
+        QuickSort(vec, start, end);
+    }
+};
+
+class printer {
+public:
+    static void print(std::vector<float> &vec) {
+                int count {0};
+        std::cout << "[";
+        std::cout << vec[count++];
+        auto size = vec.size();
+        while (count < size) {
+            std::cout << ", " << vec[count];
+            ++count;
+        }
+        std::cout << "]" << std::endl;
+    }
+};
+
+int main() {
+    std::cout << "Read README for information of limitations on program and important information." << std::endl;
+
+    controlFlags programControl;
+    std::vector<float> container;
+
+    while(programControl.programRunning) {
+        inputParser input(container, programControl);
+
+        if (!programControl.errorInProgram) {
+            randomQuickSort sorter (container);
+            printer::print(container);
+        }
+
+        // Does the user want to run another operation?
+        if (programControl.tryAgain() == 'R') {
+            container.clear();
+            programControl.programReset();
+        }
+        else
+            programControl.programRunning = false;
+
+    }
+
+    return 0;
+}
